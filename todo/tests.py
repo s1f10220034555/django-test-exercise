2889ef3b762a5826b0e8b6cfcecc90c92f65f9b3
1F10220034 <s1f102200345@iniad.org>
2023-07-06 13:37:45 +0900

    警告を修正１

--- todo/tests.py
+++ todo/tests.py
@@ -3,10 +3,11 @@ from django.utils import timezone
 from datetime import datetime
 from todo.models import Task
 
-# Create your tests here.
+
 class SampleTestCase(TestCase):
     def test_sample1(self):
         self.assertEqual(1 + 2, 3)
+
 
 class TaskModelTestCase(TestCase):
     def test_create_task1(self):
@@ -27,15 +28,15 @@ class TaskModelTestCase(TestCase):
         self.assertEqual(task.title, 'task2')
         self.assertFalse(task.completed)
         self.assertEqual(task.due_at, None)
-    
+
     def test_is_overdue_future(self):
         due = timezone.make_aware(datetime(2023, 6, 30, 23, 59, 59))
-        current = timezone.make_aware (datetime(2023, 6, 30, 0, 0, 0))
+        current = timezone.make_aware(datetime(2023, 6, 30, 0, 0, 0))
         task = Task(title='task1', due_at=due)
         task.save()
 
         self.assertFalse(task.is_overdue(current))
-    
+
     def test_is_overdue_past(self):
         due = timezone.make_aware(datetime(2023, 6, 30, 23, 59, 59))
         current = timezone.make_aware(datetime(2023, 7, 1, 0, 0, 0))
@@ -43,13 +44,14 @@ class TaskModelTestCase(TestCase):
         task.save()
 
         self.assertTrue(task.is_overdue(current))
-    
+
     def test_is_overdue_none(self):
-        current = timezone.make_aware (datetime(2023, 7, 1, 0, 0, 0))
+        current = timezone.make_aware(datetime(2023, 7, 1, 0, 0, 0))
         task = Task(title='task2')
         task.save()
 
         self.assertFalse(task.is_overdue(current))
+
 
 class TodoViewTestCase(TestCase):
     def test_index_get(self):
@@ -64,9 +66,9 @@ class TodoViewTestCase(TestCase):
         client = Client()
         data = {'title': 'Test Task', 'due_at': '2023-06-30 23:59:59'}
         response = client.post('/', data)
-        
+
         self.assertEqual(response.status_code, 200)
-        self.assertEqual (response.templates[0].name, 'todo/index.html')
+        self.assertEqual(response.templates[0].name, 'todo/index.html')
         self.assertEqual(len(response.context['tasks']), 1)
 
     def test_index_get_order_post(self):
